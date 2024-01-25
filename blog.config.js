6462d16d5d9e4b1233c9d58f10eb96c85aeb2254
stepanie1996 <130530497+stepanie1996@users.noreply.github.com>
2024-01-25 09:26:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,9 +17,9 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '91Home一站式', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '91狼友之家', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '其实你想看的，网上都有', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://flying-freighter-eb9.notion.site/91-8bc9e5750f004b6383378b240847d1cd?pvs=4', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.91home.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '91, 91home, 91狼友之家, 暗网, 呦呦, 91porn, 成人网站, 色情网站, 宅男福利, 福利姬, 强奸, 迷奸, 乱伦, 草榴, 2048, 幼幼, 缅北, 人兽', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
@@ -128,7 +128,7 @@ const BLOG = {
   BACKGROUND_DARK: '#000000', // use hex value, don't forget '#'
   SUB_PATH: '', // leave this empty unless you want to deploy in a folder
 
-  POST_SHARE_BAR_ENABLE: process.env.NEXT_PUBLIC_POST_SHARE_BAR || 'true', // 文章分享功能 ，将在底部显示一个分享条
+  POST_SHARE_BAR_ENABLE: process.env.NEXT_PUBLIC_POST_SHARE_BAR || 'false', // 文章分享功能 ，将在底部显示一个分享条
   POSTS_SHARE_SERVICES: process.env.NEXT_PUBLIC_POST_SHARE_SERVICES || 'link,wechat,qq,weibo,email,facebook,twitter,telegram,messenger,line,reddit,whatsapp,linkedin', // 分享的服務，按顺序显示,逗号隔开
   // 所有支持的分享服务：link(复制链接),wechat(微信),qq,weibo(微博),email(邮件),facebook,twitter,telegram,messenger,line,reddit,whatsapp,linkedin,vkshare,okshare,tumblr,livejournal,mailru,viber,workplace,pocket,instapaper,hatena
 
