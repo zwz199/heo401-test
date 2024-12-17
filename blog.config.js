73c94a0785ba0da846d2a917f360e2f24e28dab9
chenhaos112312 <158687105+chenhaos112312@users.noreply.github.com>
2024-12-17 20:25:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -87,6 +87,7 @@ const BLOG = {
   CUSTOM_RIGHT_CLICK_CONTEXT_MENU: process.env.NEXT_PUBLIC_CUSTOM_RIGHT_CLICK_CONTEXT_MENU || false, // 自定义右键菜单，覆盖系统菜单
 
   // 自定义外部脚本，外部样式
+  CRISP_WEBSITE_ID:'955142dd-db46-4160-8ac4-60412627ebab',
   CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
   CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
