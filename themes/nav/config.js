b59501f48a0d96148bc26d96bbc78b99449bd327
zwz199 <135852339+zwz199@users.noreply.github.com>
2024-12-29 09:20:33 +0800

    Update config.js

--- themes/nav/config.js
+++ themes/nav/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
   // 菜单
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
   // Widget
